b5c328b5b1ff2f041332c59f675304e98b845a04
Vincent-vst <louise.insomnia@gmail.com>
2021-12-02 23:26:02 +0100

    select *

--- php files/model/request.php
+++ php files/model/request.php
@@ -1,4 +1,11 @@
 <?php
-
+    
+    include_once('BDD.class.php');
+    $bdd = new BDD();     
+    
+    function test(){
+        $results = $bdd -> execQuery('select * from fkdm_medaille');
+        
+    }
 
 ?>
