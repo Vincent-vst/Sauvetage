ec64121fdf77c6a00260c175886057e1ec9c0f5a
Alexandre-Engel <alexandre.engel@etu.univ-lyon1.fr>
2021-12-02 23:42:39 +0100

    Logo

--- sauvetage-app/src/pages/Accueil.js
+++ sauvetage-app/src/pages/Accueil.js
@@ -1,10 +1,12 @@
 import Actualites from "../components/Actualites";
+import Logo from "../components/Logo";
 import Nav from "../components/Nav";
 import Pres from "../components/Pres";
 
 const Accueil = () => {
     return (
         <div className="accueil">
+            <Logo/>
             <Nav />
             <h1>Ceci est la home page</h1>
             <Pres />
