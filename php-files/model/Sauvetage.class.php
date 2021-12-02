d187c5ff3148548fec84243f436ae6c35baa8f96
Martin Bonnefoy <martin.bonnefoy@gmail.com>
2021-12-03 00:11:32 +0100

    Création de la classe Naufrage et de son DAO

---
+++ php-files/model/Sauvetage.class.php
@@ -0,0 +1,24 @@
+<?php
+
+class Sauvetage
+{
+
+
+    public function __construct(array $donnees)
+    {
+        $this->hydrate($donnees);
+    }
+
+    public function hydrate(array $donnees)
+    {
+        foreach ($donnees as $key => $value) {
+            $method = 'set' . ucfirst($key);
+
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
+    }
+
+
+}
