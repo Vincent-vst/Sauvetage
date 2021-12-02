6f26b57b5e32881421066dd7d9a81ff6e6f78ec0
Mia <daryne.benredjeb@gmail.com>
2021-12-02 21:53:41 +0100

    Update color css

---
+++ sauvetage-app/src/components/Footer.js
@@ -0,0 +1,15 @@
+import react from 'react';
+import {NavLink} from "react-router-dom"
+const Footer = () => {
+    return(
+        <div className="footer">
+            <NavLink exact activeClassName="active" to="/home">
+                Acceuil
+            </NavLink>
+            <NavLink exact activeClassName="active" to="/erreur">
+                A propos
+            </NavLink>
+        </div>
+    )
+}
+export default Navigation;
