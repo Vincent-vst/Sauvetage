9c58aa3147f94ac9934bf0a31977049d085f8378
Hugo Torres <hugotorres.elartic@gmail.com>
2021-12-02 23:25:56 +0100

    Basic stuff in accueil, pres, actualites, articles

--- sauvetage-app/src/pages/Articles.js
+++ sauvetage-app/src/pages/Articles.js
@@ -1,6 +1,9 @@
+import Nav from "../components/Nav";
+
 const Articles = () => {
     return (
         <div className="articles">
+        <Nav />
             <h1>Articles</h1>
         </div>
     )
