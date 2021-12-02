f6086bd11f0182814898ae1d29edf4b5f185ba37
Martin Bonnefoy <martin.bonnefoy@gmail.com>
2021-12-02 23:52:18 +0100

    Création de la classe BateauSauveteur et de son DAO

---
+++ php-files/model/BateauSauveteur.class.php
@@ -0,0 +1,40 @@
+<?php
+
+class BateauSauveteur extends Bateau
+{
+    protected $bateau_sauveteur_id;
+
+    public function __construct(array $donnees)
+    {
+        $this->hydrate($donnees);
+    }
+
+    public function hydrate(array $donnees)
+    {
+        foreach ($donnees as $key => $value) {
+            $method = 'set' . ucfirst($key);
+
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function get_bateau_sauveteur_id()
+    {
+        return $this->bateau_sauveteur_id;
+    }
+
+    /**
+     * @param mixed $bateau_sauveteur_id
+     */
+    public function set_bateau_sauveteur_id($bateau_sauveteur_id): void
+    {
+        $this->bateau_sauveteur_id = $bateau_sauveteur_id;
+    }
+
+
+}
---
+++ php-files/model/BateauSauveteurDAO.php
@@ -0,0 +1,41 @@
+<?php
+
+class BateauSauveteurDAO
+{
+    private $bdd;
+
+    public function __construct(BDD $bdd)
+    {
+        $this->bdd = $bdd;
+    }
+
+    public function add(BateauSauveteur $bateau)
+    {
+        $this->bdd->execQuery(
+            "INSERT INTO rids_bateau (nom, type, date_debut_utilisation, date_fin_utilisation) VALUES (?, ?, ?, ?)",
+            array($bateau->get_nom(), $bateau->get_type(), $bateau->get_date_debut_utilisation(), $bateau->get_date_fin_utilisation())
+        );
+        $bateau->set_bateau_id($this->bdd->lastInsertId());
+        $this->bdd->execQuery(
+            "INSERT INTO kdfm_bateau_sauveteur (bateau_id) VALUES (?)",
+            array($bateau->get_bateau_id())
+        );
+        $bateau->set_bateau_sauveteur_id($this->bdd->lastInsertId());
+    }
+
+    public function update(BateauSauveteur $bateau)
+    {
+        $this->bdd->execQuery(
+            "UPDATE rids_bateau SET nom = ?, type = ?, date_debut_utilisation = ?, date_fin_utilisation = ? WHERE bateau_id = ?",
+            array($bateau->get_nom(), $bateau->get_type(), $bateau->get_date_debut_utilisation(), $bateau->get_date_fin_utilisation(), $bateau->get_bateau_id())
+        );
+    }
+
+    public function delete(BateauSauveteur $bateau)
+    {
+        $this->bdd->execQuery(
+            "DELETE ON CASCADE FROM rids_bateau WHERE bateau_id = ?",
+            array($bateau->get_bateau_id())
+        );
+    }
+}
