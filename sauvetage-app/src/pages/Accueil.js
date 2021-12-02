d83ea3156d22c7d6b69a390f730a87c2d6c765a2
Alexandre-Engel <alexandre.engel@etu.univ-lyon1.fr>
2021-12-02 22:54:16 +0100

    Nav Bar

--- sauvetage-app/src/pages/Accueil.js
+++ sauvetage-app/src/pages/Accueil.js
@@ -1,6 +1,9 @@
+import Nav from "../components/Nav";
+
 const Accueil = () => {
     return (
         <div className="accueil">
+            <Nav />
             <h1>Ceci est la home page</h1>
         </div>
     );
