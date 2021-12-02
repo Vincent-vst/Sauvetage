e5243fcd4cf64f92b6c5a91b6f14cf6837a2a4d2
Alexandre-Engel <alexandre.engel@etu.univ-lyon1.fr>
2021-12-02 23:45:39 +0100

    Logo 2

--- sauvetage-app/src/components/Nav.js
+++ sauvetage-app/src/components/Nav.js
@@ -4,7 +4,6 @@ import { NavLink } from "react-router-dom";
 const Nav = () => {
     return (
         <div className="navigation">
-            <img src="./public/img/logo.png" alt="Logo" />
             <NavLink exact to="/">Accueil</NavLink>
             <NavLink exact to="/Articles">Articles</NavLink>
             <NavLink exact to="/AProros">À Propos</NavLink>
