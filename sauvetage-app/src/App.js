14158de123c0e5c657f7a128bf2b1eb909962eff
Hugo Torres <hugotorres.elartic@gmail.com>
2021-12-02 23:24:41 +0100

    adding basic stuff in Accueil

--- sauvetage-app/src/App.js
+++ sauvetage-app/src/App.js
@@ -12,6 +12,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
             <Route path="/" element={<Accueil />} />
+            <Route path="/articles" element={<Articles />} />
         </Routes>
       </BrowserRouter>
     </div>
