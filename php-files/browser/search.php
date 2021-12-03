703a54e80fe902cd5641542da0adc9b94274ceb0
Vincent-vst <louise.insomnia@gmail.com>
2021-12-03 01:58:19 +0100

    search bug

--- php-files/browser/search.php
+++ php-files/browser/search.php
@@ -3,19 +3,12 @@
 	/* echo 'hello' . '<br>'; */
 	include_once('../model/BDD.class.php');
 
-	/* $userQuerry = $_POST['searcher']; */ 
-	/* /1* $sql = "SELECT * FROM rids_bateau WHERE MATCH(nom,type) AGAINST ('%" . $userQuerry . "%')"; *1/ */ 
-	$sql = "select * from rids_bateau"; 
-	/* echo $userQuerry . '<br>'; */ 
-	/* echo "hello world"; */
+	$userQuerry = $_POST['searcher']; 
+	$sql = "SELECT * FROM rids_bateau WHERE MATCH(nom,type) AGAINST ('%" . $userQuerry . "%')"; 
 	$bdd = new BDD(); 
-
-	/* $results = $bdd -> execQuery('select * from fkdm_medaille'); */
-
 	$results = $bdd -> execQuery($sql);
-
-
-	var_dump($results);	
+	foreach ($results as $res){
+		echo $res["nom"] . "<br>";
+	}
 	
-
 ?>
