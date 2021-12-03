cdef00f9c3bb3d7a0232d59bed7087ab0899a275
Mia <daryne.benredjeb@gmail.com>
2021-12-03 01:25:41 +0100

    Footer Update

--- sauvetage-app/src/components/Footer.js
+++ sauvetage-app/src/components/Footer.js
@@ -3,23 +3,39 @@ import {NavLink} from "react-router-dom"
 const Footer = () => {
     return(
         <div className="footer">
-            <div className="footerLeft" >
+            <div className="footerCredit" >
                 <h1>Infox</h1>
+                <p>
+                   Lorem ipsum dolor sit amet, consectetur adip
+                </p>
+                <p className="textCredit"> Site sous licence ©. Tous droits réservés.</p>
             </div>
-            <div className="footerRight" >
-                <h1>
-                    <NavLink exact activeClassName="active" to="/Contact">
-                        Contact
-                    </NavLink>
-                </h1>
-                <h1>
-                <NavLink exact activeClassName="active" to="/APropos">
-                    A propos
-                </NavLink>
-                </h1>
-                <h1>
-                    Liens
-                </h1>
+            <div className="footerContact" >
+                <h1>Contact</h1>
+                <ul>
+                    <li>Mail</li>
+                    <li>Twitter</li>
+                    <li>Facebook</li>
+                    <li>Instagram</li>
+                </ul>
+            </div>
+            <div className="footerPlus">
+                <h1>En savoir plus</h1>
+                <ul>
+                    <li>A propos</li>
+                    <li></li>
+                    <li>Blub</li>
+                    <li>Blub</li>
+                </ul>
+            </div>       
+            <div className="footerLiens">
+                <h1>Liens</h1>
+                <ul>
+                    <li>Blub</li>
+                    <li>Blub</li>
+                    <li>Blub</li>
+                    <li>Blub</li>
+                </ul>
             </div>
         </div>
     )
