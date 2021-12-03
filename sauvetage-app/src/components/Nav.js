070988b2f2ed466a2cd2e74d48a8e58923f9948f
Alexandre-Engel <alexandre.engel@etu.univ-lyon1.fr>
2021-12-03 01:47:19 +0100

    Lien actif différencier des autres

--- sauvetage-app/src/components/Nav.js
+++ sauvetage-app/src/components/Nav.js
@@ -4,10 +4,10 @@ import { NavLink } from "react-router-dom";
 const Nav = () => {
     return (
         <div className="navigation">
-            <NavLink exact to="/">Accueil</NavLink>
-            <NavLink exact to="/Articles">Articles</NavLink>
-            <NavLink exact to="/AProros">À Propos</NavLink>
-            <NavLink exact to="/Communaute">Communauté</NavLink>
+            <NavLink exact to="/" className={({ isActive }) => isActive ? "red" : "blue"}>Accueil</NavLink>
+            <NavLink exact to="/Articles" className={({ isActive }) => isActive ? "red" : "blue"}>Articles</NavLink>
+            <NavLink exact to="/AProros" className={({ isActive }) => isActive ? "red" : "blue"}>À Propos</NavLink>
+            <NavLink exact to="/Communaute" className={({ isActive }) => isActive ? "red" : "blue"}>Communauté</NavLink>
         </div>
     );
 };
