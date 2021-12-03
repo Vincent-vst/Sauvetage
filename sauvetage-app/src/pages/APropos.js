6bc92725d09f723d40f517308dda8e89d9155420
Mia <daryne.benredjeb@gmail.com>
2021-12-03 04:02:47 +0100

    Update Style

--- sauvetage-app/src/pages/APropos.js
+++ sauvetage-app/src/pages/APropos.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const APropos = () => {
     return (
-        <div>
-            <h1> Ce site correspond à l'équipe INFOX </h1>
+        <div className="apropos">
+            <h2> Ce site correspond à l'équipe INFOX </h2>
             <p> Ce site a été créé lors de la nuit de l'info 2021 </p>
             <p>  INFOX: Un jeu de mot subtile entre l'informatique (l'IUT Info) et le renard en anglais ! </p>
             <p> Cette magnifique équipe est une équipe dévouée</p>
