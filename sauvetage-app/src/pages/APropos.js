ce9c24dc9b7e3eda5efd350b42cfbfa129e71c72
BERIOT MARC p2100238 <marc.beriot@etu.univ-lyon1.fr>
2021-12-03 03:22:33 +0100

    apropos

--- sauvetage-app/src/pages/APropos.js
+++ sauvetage-app/src/pages/APropos.js
@@ -6,6 +6,7 @@ const APropos = () => {
             <h1> Ce site correspond à l'équipe INFOX </h1>
             <p> Ce site a été créé lors de la nuit de l'info 2021 </p>
             <p>  INFOX: Un jeu de mot subtile entre l'informatique (l'IUT Info) et le renard en anglais ! </p>
+            <p> Cette magnifique équipe est une équipe dévouée</p>
         </div>
     );
 };
